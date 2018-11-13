daa8518496984dc4f58e420563d4ddc0dace459f
rustam013 <rustamni@code.edu.az>
2018-11-14 01:25:20 +0400

    gerg

--- WebApplication1/Data/Migrations/20181113190916_gr.Designer.cs
+++ WebApplication1/Data/Migrations/20181113190916_gr.Designer.cs
@@ -11,9 +11,10 @@ using WebApplication1.Data;
 namespace WebApplication1.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181113190916_gr")]
+    partial class gr
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
